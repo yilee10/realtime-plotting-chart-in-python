2d551d7c2b13a7a8e20b7cf529ddd9a65a27167e
yilee10 <99.yilee@gmail.com>
2023-08-27 23:21:41 +0900

    error matrix

--- test2.py
+++ test2.py
@@ -3,10 +3,10 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 
-import math
+import math 
 import random
 import winsound as sd
-
+import time
 
 pi = 3.14159265358979
 num_data_points = 50#*4 #np.linspace()기본이 50개
@@ -52,45 +52,94 @@ model.add(LSTM(128, input_shape=(10, 1)))  #input_shape=(ntime, 1)
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 ########################################### model ###########################
+#오차 행렬
+tp = 0
+tn = 0
+fp = 0
+fn = 0
 
-time = 0
+nowtime = 0
 train_cnt = 0
 after_n_train = 100
-while(1):
-    time += 1
+while(nowtime < 1500):
+    #오차행렬 구하기 위한 초기화 오류가 일어 났을 때를 P라고 하자
+    modelP = 0
+    modelN = 0
+    trueP = 0
+    trueN = 0
+
+    nowtime += 1
     ### 그래프 이동하게###
-    if (time>100): ## 그래프 창에 100개의 점만 표시되도록
+    if (nowtime>100): ## 그래프 창에 100개의 점만 표시되도록
         time_tar_pred.pop(0)
         val_target.pop(0)
         val_predict.pop(0)
-    if (time > 100): #오차는 100개 점 봄
+    if (nowtime > 100): #오차는 100개 점 봄
         time_error.pop(0)
         val_loss.pop(0)
 
     ### 값 입력 ###
     #ax0
-    time_tar_pred.append(time)
+    time_tar_pred.append(nowtime)
     rand_choice = 0
-    if (time > 500): # time 500 이상 됐을 때 오류 생긴다고 하자
+    if (nowtime > 500): # nowtime 500 이상 됐을 때 오류 생긴다고 하자
         ax1.set_ylim([-0.001, 0.01])
-        random_y = 0.1*random.gauss(1, 1)
-        rand_list = [0 for i in range(29)]
+        random_y = 0.1*random.gauss(0, 1)
+        rand_list = [0 for i in range(10)]
         rand_list.append(random_y)
         rand_choice = random.choice(rand_list)
         if(rand_choice != 0):
-            print("Error")
-            sd.Beep(2000, 3000)
-    target = sin_y[time%50] + rand_choice
+            print("===========================")
+            #print("Error",rand_choice)           
+            trueP = 1
+            #sd.Beep(2000, 3000)
+        else: # 실제값이 이상없는 값 일경우
+            trueN = 1
+    target = sin_y[nowtime%50] + rand_choice
     val_target.append(target)
-    if(time < after_n_train+2):
+    if(nowtime < after_n_train+2):
         val_predict.append(-1.1)
     #ax1
-    time_error.append(time)
+    time_error.append(nowtime)
     errorMSE = (1.0/2.0)*(val_target[-1]-val_predict[-1])**2
     val_loss.append(errorMSE)
+    # error 가 기준 값 이상일 때 이상으로 감지
+    if (nowtime > 500):
+        print("errorMSE",errorMSE)
+        errorThreshold = 0.0005
+        if(errorMSE>errorThreshold):
+            modelP = 1
+        else:
+            modelN = 1
+
+        if(modelP==1 and trueP == 1):
+            tp += 1
+        elif(modelP == 1 and trueN == 1):
+            fp += 1
+        elif(modelN == 1 and trueN == 1):
+            tn += 1
+        elif(modelN == 1 and trueP == 1):
+            fn += 1
+        
+        print(modelP,modelN,trueP,trueN)
+        print(tp,fp,tn,fn)
+        print("nowtime",nowtime)
+        if(tp+tn+fp+fn != 0):
+            print("Accuracy",float(tp+tn)/float(tp+tn+fp+fn)) # 전체 예측 중 맞게 예측
+        if(tp+fn != 0):
+            print("Recall,Sensitivity",float(tp)/float(tp+fn)) # 실제 오류 즁 오류라고 예측한 것
+        if(tp+fp != 0):
+            print("percision",float(tp)/float(tp+fp)) # 오류라고 예측한 것 중 실제 오류
+        print("--------------------------")
+        if(fp+tn != 0):
+            print("Specificity",float(tn)/float(fp+tn)) # 오류가 아니라고 예측 한 것 중 정말 오류가 아닌 것
+        print("===========================")
+        #time.sleep(2)
+        #if(rand_choice != 0):
+            #time.sleep(8)
 
     train_cnt += 1
-    if time > after_n_train: #100번이후로 train 진행
+    if nowtime > after_n_train: #100번이후로 train 진행
 
         ###그래프 나타내기###
         line_target.set_xdata(time_tar_pred)
@@ -112,7 +161,7 @@ while(1):
         plt.pause(0.01)
 
          # train
-        if train_cnt > 30: # 30번 마다 업데이트
+        if train_cnt > 10 and nowtime < 500: # 10번 마다 업데이트 500번 까만
             #print("Update")
             ntime = 10
             nsample = len(val_target)-ntime
